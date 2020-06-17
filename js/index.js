b2467ab3549a9dd2968b6851c6cad7c88b6b1d86
Deepa Bharadwaj <deepb@umich.edu>
2020-06-17 18:50:26 -0400

    Updates with Javascript

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function replace(location, image)
+{
+    location.src = image;
+    // $(image).fadeTo('slow' , '0.33' );
+    // $("#image").stop().animate({opacity: 0},1000,function(){
+    //     $(this).css({'background-image': "url('/images/alt_image.png')"})
+    //                .animate({opacity: 1},{duration:1000});
+    //  });
+    
+}
