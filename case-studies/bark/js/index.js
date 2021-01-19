9209694214e010dde9fc8d39aad583fe0bde79d5
Deepa Bharadwaj <deepb@umich.edu>
2021-01-19 02:14:06 -0500

    finish bark and make better nav bar

--- case-studies/bark/js/index.js
+++ case-studies/bark/js/index.js
@@ -1,5 +1,6 @@
 function BlurCarouselImg(item_id) {
     var item = document.getElementById(item_id);
+    item.style.transition = "all 1.5s";
     item.style.transform = "scale(1.05)";
     item.style.filter = "blur(5px)";
 }
@@ -10,3 +11,13 @@ function ResetCarouselImg(item_id) {
     item.style.filter = "blur(0px)";
 
 }
+
+function initStickyHero(hero) {
+    var observer = new IntersectionObserver(stickyCallback.bind(hero), {threshold: [0, 0.1, 1]});
+    observer.observe(hero.content);
+  };
+  
+  function stickyCallback(entries) {
+    var bool = entries[0].intersectionRatio > 0;
+    Util.toggleClass(this.element, 'sticky-hero--media-is-fixed', bool);
+  };
