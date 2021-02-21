a1adc0386a06dee34efed0fa8a529fad267f8e79
Deepa Bharadwaj <deepb@umich.edu>
2021-02-21 01:58:19 -0500

    image carousal custom

--- case-studies/amazon/js/index.js
+++ case-studies/amazon/js/index.js
@@ -61,3 +61,74 @@ var observer = new IntersectionObserver(function(entries) {
 }, { threshold: [0.1, 1] });
 
 observer.observe(document.querySelector("#chatwindow"));
+
+
+var state = 2;
+function test_func(){
+    var img1 = document.getElementById("img_holder_1_1");
+    var img2 = document.getElementById("img_holder_1_2");
+    var img3 = document.getElementById("img_holder_1_3");
+    var caption = document.getElementById("img_holder_1_caption");
+    switch(state){
+        case 0:
+            first_click(img2, img1, img3, 2, 1);
+            caption.innerHTML = "Image 1"
+            state = 1;
+            break;
+        case 1:
+            first_click(img1, img3, img2, 0, 2);
+            caption.innerHTML = "Image 3"
+            state = 2;
+            break;
+        case 2:
+            first_click(img3, img2, img1, 1, 0);
+            caption.innerHTML = "Image 2"
+            state = 0;
+            break;
+        default:
+            break;
+    }
+}
+function right_click(){
+    state +=1;
+    state %=3;
+    test_func();
+}
+let l_btn = document.getElementById("img_holder_1_left");
+l_btn.addEventListener("mousedown", test_func);
+
+let r_btn = document.getElementById("img_holder_1_right");
+r_btn.addEventListener("mousedown", right_click);
+
+
+function first_click(elem1, elem2, elem3, click_state1, click_state3){
+    elem1.classList.remove("img_holder_active_3");
+    elem1.classList.add("img_holder_inactive");
+    elem1.parentElement.style.zIndex ="1";
+    elem1.onclick = function(){
+        state = click_state1;
+        test_func();
+    }
+
+    elem3.classList.remove("img_holder_active_3");
+    elem3.classList.add("img_holder_inactive");
+    elem3.parentElement.style.zIndex ="1";
+    elem3.onclick = function(){
+        state = click_state3;
+        test_func();
+    }
+
+    elem2.classList.remove("img_holder_inactive");
+    elem2.classList.add("img_holder_active_3");
+    elem2.parentElement.style.zIndex ="101";
+    elem2.onclick = second_click;
+}
+
+function second_click(){
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+
+test_func()
