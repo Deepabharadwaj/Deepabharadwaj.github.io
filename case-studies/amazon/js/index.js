63eb9502df3d3a6c8b819d5243a22348007a3f2e
Deepa Bharadwaj <deepb@umich.edu>
2021-02-28 17:47:58 -0500

    youtube videos

--- case-studies/amazon/js/index.js
+++ case-studies/amazon/js/index.js
@@ -1,66 +1,66 @@
-var reset = false;
-var dispayed = false;
-const updatebox3 = async () => {
-    if(reset === false)
-    {
-        var first = document.getElementById("chat2");
-        first.style.display = "none";
-        var second = document.getElementById("chat2_new");
-        second.style.display = "block";
-        dispayed = true;
-    }
-}
+// var reset = false;
+// var dispayed = false;
+// const updatebox3 = async () => {
+//     if(reset === false)
+//     {
+//         var first = document.getElementById("chat2");
+//         first.style.display = "none";
+//         var second = document.getElementById("chat2_new");
+//         second.style.display = "block";
+//         dispayed = true;
+//     }
+// }
 
-const updatebox2 = async () => {
-    if(reset === false)
-    {
-        var first = document.getElementById("chat2");
-        first.style.display = "block";
-        setTimeout(updatebox3, 2000);
-    }
-}
+// const updatebox2 = async () => {
+//     if(reset === false)
+//     {
+//         var first = document.getElementById("chat2");
+//         first.style.display = "block";
+//         setTimeout(updatebox3, 2000);
+//     }
+// }
 
-const updatebox1 = async () => {
-    if(reset === false)
-    {
-        var first = document.getElementById("chat1");
-        first.style.display = "none";
-        var second = document.getElementById("chat1_new");
-        second.style.display = "block";
-        setTimeout(updatebox2, 500);
-    }
-}
+// const updatebox1 = async () => {
+//     if(reset === false)
+//     {
+//         var first = document.getElementById("chat1");
+//         first.style.display = "none";
+//         var second = document.getElementById("chat1_new");
+//         second.style.display = "block";
+//         setTimeout(updatebox2, 500);
+//     }
+// }
 
-function chatupdate() {
-    if(dispayed === false)
-    {
-        reset = false;
-        setTimeout(updatebox1, 2000);
-    }
-}
+// function chatupdate() {
+//     if(dispayed === false)
+//     {
+//         reset = false;
+//         setTimeout(updatebox1, 2000);
+//     }
+// }
 
-function chatreset() {
-    var first = document.getElementById("chat1");
-    first.style.display = "block";
-    var second = document.getElementById("chat1_new");
-    second.style.display = "none";
-    var third = document.getElementById("chat2");
-    third.style.display = "none";
-    var fourth = document.getElementById("chat2_new");
-    fourth.style.display = "none";
-    reset = true;
-    dispayed = false;
-}
+// function chatreset() {
+//     var first = document.getElementById("chat1");
+//     first.style.display = "block";
+//     var second = document.getElementById("chat1_new");
+//     second.style.display = "none";
+//     var third = document.getElementById("chat2");
+//     third.style.display = "none";
+//     var fourth = document.getElementById("chat2_new");
+//     fourth.style.display = "none";
+//     reset = true;
+//     dispayed = false;
+// }
 
-var observer = new IntersectionObserver(function(entries) {
-    if(entries[0].isIntersecting === true && entries[0]['intersectionRatio'] > 0.5){
-      chatupdate();
-    } else {
-      chatreset();
-    }
-}, { threshold: [0.1, 1] });
+// var observer = new IntersectionObserver(function(entries) {
+//     if(entries[0].isIntersecting === true && entries[0]['intersectionRatio'] > 0.5){
+//       chatupdate();
+//     } else {
+//       chatreset();
+//     }
+// }, { threshold: [0.1, 1] });
 
-observer.observe(document.querySelector("#chatwindow"));
+// observer.observe(document.querySelector("#chatwindow"));
 
 
 var img_holder_1_state = 2;
