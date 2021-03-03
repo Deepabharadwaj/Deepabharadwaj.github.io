7feea08c1c27de57432f53370c8a98f3f26a945b
Sergey Nazarov <Scorpovi4@gmail.com>
2021-03-03 21:20:42 +0200

    about page

--- js/index.js
+++ js/index.js
@@ -854,74 +854,77 @@ window.addEventListener('load', function () {
   window.addEventListener('scroll', allInView);
 });
 
-var sliderElement = document.getElementById('carousel-reco');
-var interval = 0;
-
-function autoplay(run) {
-  clearInterval(interval);
-  interval = setInterval(() => {
-    if (run && slider) {
-      slider.next();
-    }
-  }, 7000);
-}
-
-var slider = new KeenSlider(sliderElement, {
-  loop: true,
-  spacing: 20,
-  duration: 1500,
-  dragStart: () => {
-    autoplay(false);
-  },
-  dragEnd: () => {
-    autoplay(true);
-  },
-  created: function (instance) {
-    document
-      .getElementById('arrow-left')
-      .addEventListener('click', function () {
-        instance.prev();
+if($('#carousel-reco').length > 0) {
+  var sliderElement = document.getElementById('carousel-reco');
+  var interval = 0;
+  
+  function autoplay(run) {
+    clearInterval(interval);
+    interval = setInterval(() => {
+      if (run && slider) {
+        slider.next();
+      }
+    }, 7000);
+  }
+  
+  var slider = new KeenSlider(sliderElement, {
+    loop: true,
+    spacing: 20,
+    duration: 1500,
+    dragStart: () => {
+      autoplay(false);
+    },
+    dragEnd: () => {
+      autoplay(true);
+    },
+    created: function (instance) {
+      document
+        .getElementById('arrow-left')
+        .addEventListener('click', function () {
+          instance.prev();
+        });
+  
+      document
+        .getElementById('arrow-right')
+        .addEventListener('click', function () {
+          instance.next();
+        });
+      var dots_wrapper = document.getElementById('dots');
+      var slides = document.querySelectorAll('.keen-slider__slide');
+      slides.forEach(function (t, idx) {
+        var dot = document.createElement('button');
+        dot.classList.add('dot');
+        dots_wrapper.appendChild(dot);
+        dot.addEventListener('click', function () {
+          instance.moveToSlide(idx);
+        });
       });
-
-    document
-      .getElementById('arrow-right')
-      .addEventListener('click', function () {
-        instance.next();
-      });
-    var dots_wrapper = document.getElementById('dots');
-    var slides = document.querySelectorAll('.keen-slider__slide');
-    slides.forEach(function (t, idx) {
-      var dot = document.createElement('button');
-      dot.classList.add('dot');
-      dots_wrapper.appendChild(dot);
-      dot.addEventListener('click', function () {
-        instance.moveToSlide(idx);
-      });
-    });
-    updateClasses(instance);
-  },
-  slideChanged(instance) {
-    updateClasses(instance);
-  },
-});
-
-autoplay(true);
-
-function updateClasses(instance) {
-  var slide = instance.details().relativeSlide;
-  var arrowLeft = document.getElementById('arrow-left');
-  var arrowRight = document.getElementById('arrow-right');
-  slide === 0
-    ? arrowLeft.classList.add('arrow--disabled')
-    : arrowLeft.classList.remove('arrow--disabled');
-  slide === instance.details().size - 1
-    ? arrowRight.classList.add('arrow--disabled')
-    : arrowRight.classList.remove('arrow--disabled');
-
-  var dots = document.querySelectorAll('.dot');
-  dots.forEach(function (dot, idx) {
-    idx === slide
-      ? dot.classList.add('dot--active')
-      : dot.classList.remove('dot--active');
+      updateClasses(instance);
+    },
+    slideChanged(instance) {
+      updateClasses(instance);
+    },
   });
+  
+  autoplay(true);
+  
+  function updateClasses(instance) {
+    var slide = instance.details().relativeSlide;
+    var arrowLeft = document.getElementById('arrow-left');
+    var arrowRight = document.getElementById('arrow-right');
+    slide === 0
+      ? arrowLeft.classList.add('arrow--disabled')
+      : arrowLeft.classList.remove('arrow--disabled');
+    slide === instance.details().size - 1
+      ? arrowRight.classList.add('arrow--disabled')
+      : arrowRight.classList.remove('arrow--disabled');
+  
+    var dots = document.querySelectorAll('.dot');
+    dots.forEach(function (dot, idx) {
+      idx === slide
+        ? dot.classList.add('dot--active')
+        : dot.classList.remove('dot--active');
+    });
+  }
+  
 }
